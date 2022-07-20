1dfb45b3c8f6a3708061b27d0818d7e179237640
yuliangzhong <yubaby2003@163.com>
2022-07-20 21:36:33 +0200

    score: 110

--- flappy_automation_code/scripts/flappy_automation_code_node.py
+++ flappy_automation_code/scripts/flappy_automation_code_node.py
@@ -17,34 +17,49 @@ LASER_NUM = 9
 LASER_RANGE = 3.549 # m
 ANGLES = np.linspace(-45,45,9)/180*math.pi # radius
 
-OBSTACLE_WIDTH_SUP = 0.60 # m
 OBSTACLE_GAP = 1.92 # m
 GATE_HEIGHT = 0.50 # m
+
+# my constants
+OBSTACLE_WIDTH_SUP = 0.60 # m
 UP_LOW_FENCE = 0.7 # m
 
 T_REACH_MAX = 0.65 # s
+FREE_X = 1.05 # m
+VX_SAFE = FREE_X / T_REACH_MAX + AX_MAX * T_REACH_MAX / 2
 
 # y-axis MPC control constants
-N = 20
+N = 25
+# x_+ = Ax + Bu
 A = np.array([[1, -DELTA_T], [0, 1]])
 B = np.array([[-DELTA_T**2/2],[DELTA_T]])
 
+# Ax*x_i <= bx; Au*u_i <= bu; Af*x_N <= bf
+Au = np.array([[1], [-1]])
+# Loss = |xN|_P + sum(|xi|_Q + |ui|_R)
+
+# with substitution
+# Sx = [[I], [A], [A^2], ..., [A^N]]
+# Su = [[0, ..., 0], [B, 0, ..., 0], [AB, B, 0, ..., 0], ..., [A^(N-1)B, ..., AB, B]]
 Sx = np.zeros((2*N+2, 2))
 Su = np.zeros((2*N+2, N))
-for iSx in range(N+1):
-    Sx[2*iSx:2*iSx+2, :] = np.linalg.matrix_power(A, iSx)
-    for iSu in range(iSx):
-        Su[2*iSx:2*iSx+2, iSu:iSu+1] = np.dot(np.linalg.matrix_power(A, iSx-1-iSu), B)
+for i in range(N+1):
+    Sx[2*i:2*i+2, :] = np.linalg.matrix_power(A, i)
+    for j in range(i):
+        Su[2*i:2*i+2, j:j+1] = np.dot(np.linalg.matrix_power(A, i-j-1), B)
 
+# Q_bar = diag(Q, Q, ..., Q, P)
+# R_bar = diag(R, R, ..., R)
 Q_bar = np.diag(np.zeros(2*N+2))
-for iQ_bar in range(N+1):
-    Q_bar[2*iQ_bar:2*iQ_bar+2, 2*iQ_bar:2*iQ_bar+2] = np.array([[iQ_bar,0],[0,0.01]])
+for i in range(N+1):
+    Q_bar[2*i:2*i+2, 2*i:2*i+2] = np.array([[i,0],[0,0.01]])
 R_bar = np.diag(np.zeros(N))
 
+# constraints: G*x <= w + E*x(0)
 h = AY_MAX*np.ones((2*N,1))
 G = np.zeros((2*N, N))
-for iG in range(N):
-    G[2*iG:2*iG+2, iG:iG+1] = np.array([[1], [-1]])
+for i in range(N):
+    G[2*i:2*i+2, i:i+1] = Au
 
 H = np.dot(np.dot(Su.T, Q_bar), Su) + R_bar
 F = np.dot(np.dot(Sx.T, Q_bar), Su)
@@ -67,7 +82,7 @@ class Agent(object):
 
         # define the y-axis goal
         self.goal_y = 0.0
-        self.goal_y_reach_t = 0.0
+        self.goal_y_reach_count = 0
 
         # position up bound and low bound
         self.pos_up = 0.0
@@ -92,8 +107,8 @@ class Agent(object):
         # the bird status
         self.status = 'init' # 'init', 'free', 'constrained', 'forecast'
 
-        # forecast time
-        self.forecast_time = 0.0
+        # forecast count
+        self.forecast_count = 0.0
 
         # print_out
         self.time_count = 0
@@ -104,6 +119,8 @@ class Agent(object):
 
 
     def laserScanCallback(self, msg):
+        
+        start_time = time.time()
         
         # position integral
         self.pos_x += self.dx
@@ -123,6 +140,20 @@ class Agent(object):
         # take actions based on current bird status
         self.setGoalY()
         self.setAcc()
+
+        end_time = time.time()
+        # print("Total time cost %.7f" % ((end_time - start_time))) # < 2ms
+
+
+    def obstacleMemoryReset(self, flag):
+        if flag == 'f':
+            self.forward_obstacles = np.zeros(self.resolution)
+            self.forward_max_x = 0
+            self.forward_min_x = 999
+        elif flag == 'b':
+            self.backward_obstacles = np.zeros(self.resolution)
+            self.backward_max_x = 0
+            self.backward_min_x = 999
 
 
     def setUpLowBound(self, msg):
@@ -220,13 +251,15 @@ class Agent(object):
         elif self.pos_x > self.front_obstacle_max_x:
             self.status = 'free'
             self.front_obstacle_update_flag = 1
+            self.front_obstacle_max_x = self.forward_max_x + 0.25
+            self.front_obstacle_min_x = self.forward_min_x - 0.20
         
         print("The bird status is", self.status)
 
 
     def setGoalY(self):
 
-        if self.status == 'constrained' or self.status == 'init':
+        if self.status == 'init':
             return
         elif self.status == 'free' or self.status == 'forecast':
 
@@ -259,23 +292,35 @@ class Agent(object):
   
             # print(zero_count, zero_best)
 
-            if self.status == 'free':
+            if self.status == 'free' or self.status == 'constrained':
                 self.goal_y = goal_y
             if self.status == 'forecast':
-                _, self.forecast_time = self.calculateAccY(self.vel_buffer.y, goal_y)
-                print("forcast time %.3f, goal_y %.3f" %(self.forecast_time, goal_y))
-
-
-    def obstacleMemoryReset(self, flag):
-        if flag == 'f':
-            self.forward_obstacles = np.zeros(self.resolution)
-            self.forward_max_x = 0
-            self.forward_min_x = 999
-        elif flag == 'b':
-            self.backward_obstacles = np.zeros(self.resolution)
-            self.backward_max_x = 0
-            self.backward_min_x = 999
+                _, self.forecast_count = self.calculateAccY(self.vel_buffer.y, goal_y)
+                print("forcast count %.3f, goal_y %.3f" %(self.forecast_count, goal_y))
             
+
+    def setAcc(self):
+
+        # setup accelerations
+        # acc_x = -0.5
+        # acc_y = 0.0
+        acc_y, self.goal_y_reach_count = self.calculateAccY(self.vel_buffer.y, self.goal_y)
+        acc_x = self.calculateAccX()
+        self.pub_acc_cmd.publish(Vector3(acc_x, acc_y, 0))
+        
+        # log dx dy in this time step
+        # acc_x, acc_y should satisfy constraints
+        acc_x = max(min(acc_x, AX_MAX), -AX_MAX)
+        acc_y = max(min(acc_y, AY_MAX), -AY_MAX)
+        # Vx >= 0
+        Vx_p = max(self.vel_buffer.x + acc_x*DELTA_T, 0.0)
+        self.dx = (self.vel_buffer.x + Vx_p) * DELTA_T / 2
+        self.dy = self.vel_buffer.y*DELTA_T + acc_y*DELTA_T*DELTA_T/2
+
+        t = self.time_count * DELTA_T
+        print("t: %.3f, x: %.2f, y: %.2f, Vx: %.2f, Vy: %.2f, ax: %.2f, ay: %.2f, goal_y: %.2f, t_reach: %.3f" % (t, self.pos_x, self.pos_y, self.vel_buffer.x, self.vel_buffer.y, acc_x, acc_y, self.goal_y, self.goal_y_reach_count*DELTA_T))
+        self.time_count += 1
+
 
     def calculateAccY(self, Vy, goal_y):
         # solve y-axis acceleration by MPC --> QP
@@ -290,67 +335,135 @@ class Agent(object):
         xs = np.dot(Sx,x0) + np.dot(Su,u)
         xs = xs.reshape(2*N+2)[::2]
         try:
-            n_time_gaps = N - np.where(np.abs(xs)[::-1]>0.02)[0][0] + 1
-            t_approx = n_time_gaps*DELTA_T
+            n_time_gaps = N - np.where(np.abs(xs)[::-1]>0.015)[0][0] + 1
         except:
-            t_approx = 0.0
+            n_time_gaps = 0
         # end_time = time.time()
         # print("QP time cost %.7f" % ((end_time - starttime))) # about 0.8ms
-        return ay, t_approx
+        return ay, n_time_gaps
 
 
-    def calculateAccX(self, Vx, t_approx):
-        
-        # set Vx = 2 m/s before game starts
-        if self.front_obstacle_min_x == 999 and Vx < 2:
-            ax = 0.5
-        elif self.front_obstacle_min_x == 999 and Vx >= 2:
-            ax = 0.0
-        else:
-            if self.pos_x > self.front_obstacle_min_x and self.pos_x < self.front_obstacle_max_x:
-                V_out = 3.3 # m/s
-                Dx = self.front_obstacle_max_x - self.pos_x
-                S = (Vx**2 - V_out**2)/2/AX_MAX+ 2*(Vx*DELTA_T + AX_MAX*DELTA_T*DELTA_T/2) # [m]
-                if Dx > S:
-                    ax = AX_MAX
-                else:
-                    ax = -AX_MAX
+    def calculateAccX(self):
+
+        if self.status == 'init':
+            if self.vel_buffer.x < 3.0:
+                ax = 1.5
             else:
-                Dx = self.front_obstacle_min_x - self.pos_x
-                S = Vx * t_approx
-                if S <= Dx:
-                    ax = 2 * (Dx - S) / (t_approx**2)
-                elif S <= 2*Dx:
-                    ax = 2 * (Dx - S) / (t_approx**2)
-                else:
-                    ax = -AX_MAX
-                    print("bad!")
+                ax = 0.0
 
-        return max(min(ax, AX_MAX), -AX_MAX)
-
-    def setAcc(self):
-
-        # setup accelerations
-        acc_x = -0.5
-        # acc_y = 0.0
-        acc_y, self.goal_y_reach_t = self.calculateAccY(self.vel_buffer.y, self.goal_y)
-        # acc_x = self.calculateAccX(msg.x, t_approx)
-        self.pub_acc_cmd.publish(Vector3(acc_x, acc_y, 0))
+        elif self.status == 'free':
+            if self.goal_y_reach_count > 0:
+                ax = self.onlineMpcSolver(self.pos_x, self.vel_buffer.x, self.goal_y_reach_count, self.front_obstacle_min_x)
+            else:
+                ax = AX_MAX
         
-        # log dx dy in this time step
-        # acc_x, acc_y should satisfy constraints
-        acc_x = max(min(acc_x, AX_MAX), -AX_MAX)
-        acc_y = max(min(acc_y, AY_MAX), -AY_MAX)
-        # Vx >= 0
-        Vx_p = max(self.vel_buffer.x + acc_x*DELTA_T, 0.0)
-        self.dx = (self.vel_buffer.x + Vx_p) * DELTA_T / 2
-        self.dy = self.vel_buffer.y*DELTA_T + acc_y*DELTA_T*DELTA_T/2
+        elif self.status == 'constrained':
+            Dv = VX_SAFE - self.vel_buffer.x
+            ax = max(min(Dv / DELTA_T, AX_MAX), -AX_MAX)
 
-        t = self.time_count * DELTA_T
-        print("t: %.3f, x: %.2f, y: %.2f, Vx: %.2f, Vy: %.2f, ax: %.2f, ay: %.2f, goal_y: %.2f, t_reach: %.3f" % (t, self.pos_x, self.pos_y, self.vel_buffer.x, self.vel_buffer.y, acc_x, acc_y, self.goal_y, self.goal_y_reach_t))
-        self.time_count += 1
+        elif self.status == 'forecast':
+            if self.forecast_count > 0:
+                t = self.forecast_count*DELTA_T
+                Vx_desire = FREE_X/t + AX_MAX*t/2
+                print("Vx desire = %.2f" % Vx_desire)
+                Dv = Vx_desire - self.vel_buffer.x
+                ax = max(min(Dv / DELTA_T, AX_MAX), -AX_MAX)
+            else:
+                ax = AX_MAX
+        # else:
+        #     if self.pos_x > self.front_obstacle_min_x and self.pos_x < self.front_obstacle_max_x:
+        #         V_out = 3.3 # m/s
+        #         Dx = self.front_obstacle_max_x - self.pos_x
+        #         S = (Vx**2 - V_out**2)/2/AX_MAX+ 2*(Vx*DELTA_T + AX_MAX*DELTA_T*DELTA_T/2) # [m]
+        #         if Dx > S:
+        #             ax = AX_MAX
+        #         else:
+        #             ax = -AX_MAX
+        #     else:
+        #         Dx = self.front_obstacle_min_x - self.pos_x
+        #         S = Vx * t_approx
+        #         if S <= Dx:
+        #             ax = 2 * (Dx - S) / (t_approx**2)
+        #         elif S <= 2*Dx:
+        #             ax = 2 * (Dx - S) / (t_approx**2)
+        #         else:
+        #             ax = -AX_MAX
+        #             print("bad!")
 
+        # return max(min(ax, AX_MAX), -AX_MAX)
+        return ax
+
+
+    def onlineMpcSolver(self, px, vx, N, r):
+
+        print("N = %.1f, r = %.3f" % (N, r))
+        # x_+ = Ax + Bu
+        A = np.array([[1, DELTA_T], [0, 1]])
+        B = np.array([[DELTA_T**2/2],[DELTA_T]])
+        # Ax*x_i <= bx; Au*u_i <= bu; Af*x_N <= bf
+        Ax = np.array([[0, -1]])
+        # bx = np.array([[0]])
+        Au = np.array([[1], [-1]])
+        # bu = np.array([[AX_MAX], [AX_MAX]])
+        Af = np.array([[1, 0],[0, -1]])
+        # bf = np.array([[0], [0]])
+        # Loss = |xN|_P + sum(|xi|_Q + |ui|_R)
+        # P = np.array([[1, 0], [0, 0]])
+        # Q = np.zeros((2,2))
+        # R = np.array([[0]])
+
+        # with substitution
+        # Sx = [[I], [A], [A^2], ..., [A^N]]
+        # Su = [[0, ..., 0], [B, 0, ..., 0], [AB, B, 0, ..., 0], ..., [A^(N-1)B, ..., AB, B]]
+        Sx = np.zeros((2*N+2, 2))
+        Su = np.zeros((2*N+2, N))
+        for i in range(N+1):
+            Sx[2*i:2*i+2, :] = np.linalg.matrix_power(A, i)
+            for j in range(i):
+                Su[2*i:2*i+2, j:j+1] = np.dot(np.linalg.matrix_power(A, i-j-1), B)
+
+        # Q_bar = diag(Q, Q, ..., Q, P)
+        # R_bar = diag(R, R, ..., R)
+        Q_bar = np.diag(np.zeros(2*N+2))
+        Q_bar[-2, -2] = 1
+        R_bar = np.diag(np.zeros(N))
+
+        # constraints: G*x <= w + E*x(0)
+        Gu = np.zeros((2*N, N))
+        for i in range(N):
+            Gu[2*i:2*i+2, i:i+1] = Au
+        AX = np.zeros((N, 2*N))
+        for i in range(N):
+            AX[i:i+1, 2*i:2*i+2] = Ax
+        Gx = np.dot(AX, Su[0:2*N, :])
+        Gf = np.dot(Af, Su[2*N:2*N+2, :])
+        G = np.vstack((Gu, Gx, Gf))
+
+        wu = AX_MAX*np.ones((2*N, 1))
+        wx = np.zeros((N, 1))
+        wf = np.zeros((2,1))
+        w = np.vstack((wu, wx, wf))
+
+        Eu = np.zeros((2*N, 2))
+        Ex = -np.dot(AX, Sx[0:2*N, :])
+        Ef = -np.dot(Af, Sx[2*N:2*N+2, :])
+        E = np.vstack((Eu, Ex, Ef))
+
+        H = np.dot(np.dot(Su.T, Q_bar), Su) + R_bar
+        F = np.dot(np.dot(Sx.T, Q_bar), Su)
+
+        x0 = np.array([[px-r], [vx]])
+        h = w + np.dot(E, x0)
+        q = np.dot(F.T, x0)
+        u = solve_qp(H, q, G, h, solver="osqp")
         
+        print("solution: u = {}".format(u))
+        try:
+            ax = u[0]
+        except:
+            ax = -AX_MAX
+        return ax
+
 
 if __name__ == '__main__':
     try:
